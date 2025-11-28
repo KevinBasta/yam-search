7489b27cdbb4ea8ff0e9d18e396cacfc87a75932
Kevin Basta <kevinbastadeveloper@gmail.com>
2025-11-28 16:34:35 -0500

    go indexer start

--- indexer/indexer.go
+++ indexer/indexer.go
@@ -1,39 +1,75 @@
-package indexer
+package main
 
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "modernc.org/sqlite" // Import the SQLite driver
 )
 
 type Document struct {
 	docId int
+	url   string
 	title string
 	body  string
 }
 
-func (doc *Document) getNextDocument(db *sql.DB) {
+func (doc *Document) getNextDocument(db *sql.DB) error {
 	doc.docId++
+
+	docId := 0
+	pagerank := 0
+
 	row := db.QueryRow("SELECT * FROM docIdToData WHERE docId = ?", doc.docId)
-	//err := row.Scan()
+	err := row.Scan(&docId, &doc.url, &doc.title, &doc.body, &pagerank)
+	if err != nil {
+		*doc = Document{doc.docId, "", "", ""}
+		return err
+	}
+
+	return nil
 }
 
-func createIndex() {
-	db, err := sql.Open("sqlite", "/out/document_collection.db")
+func (doc *Document) index(db *sql.DB) error {
+	fmt.Println("Indexing ", doc.docId)
+	return nil
+}
+
+func createIndex(inDB string, outDB string) error {
+	// Open db containing crawled data
+	db, err := sql.Open("sqlite", inDB)
 	defer db.Close()
 	if err != nil {
-		fmt.Println("Error opening document collection db")
-		return
+		return err
 	}
 
-	doc := Document{0, "", ""}
+	// Create db for index we generate
+	os.Create(outDB)
+	db2, err2 := sql.Open("sqlite", outDB)
+	defer db2.Close()
+	if err2 != nil {
+		return err
+	}
+
+	doc := Document{0, "", "", ""}
 	for {
-		doc.getNextDocument(db)
+		err = doc.getNextDocument(db)
+		if err != nil {
+			fmt.Println(err)
+			fmt.Println("Finished indexing")
+			break
+		}
 
+		doc.index(db2)
 	}
+
+	return nil
 }
 
 func main() {
+	inDB := "../out/document_collection.db"
+	outDB := "../out/index.db"
 
+	createIndex(inDB, outDB)
 }
--- viewsql.py
+++ viewsql.py
@@ -2,7 +2,7 @@ import sqlite3
 
 conn = sqlite3.connect('out/document_collection.db')
 cursor = conn.cursor()
-cursor.execute('SELECT * FROM docIdToUrlAndBody;')
+cursor.execute('SELECT * FROM docIdToData;')
 rows = cursor.fetchall()
 
 print("printing now")
