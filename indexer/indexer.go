d5cff2cc5f507acfa2bb883fe8da5b9db91bed3d
Kevin Basta <kevinbastadeveloper@gmail.com>
2025-11-28 17:25:47 -0500

    stop word removal and word stemming in indexer

--- indexer/indexer.go
+++ indexer/indexer.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"database/sql"
 	"fmt"
 	"os"
+	"strings"
 
+	"github.com/blevesearch/snowballstem"
+	"github.com/blevesearch/snowballstem/english"
 	_ "modernc.org/sqlite" // Import the SQLite driver
 )
+
+var stopWords = make(map[string]int)
+var snowballEnv = snowballstem.NewEnv("")
 
 type Document struct {
 	docId int
@@ -31,45 +38,108 @@ func (doc *Document) getNextDocument(db *sql.DB) error {
 	return nil
 }
 
-func (doc *Document) index(db *sql.DB) error {
+func (doc *Document) index(idb *sql.DB, ddb *sql.DB) error {
 	fmt.Println("Indexing ", doc.docId)
+
+	var wordToFreqency = make(map[string]int)
+
+	// loop through words in body of document
+	words := strings.Fields(doc.body)
+	for _, word := range words {
+		// skip this word if it's a stop word
+		_, isStopWord := stopWords[word]
+		if isStopWord {
+			continue
+		}
+
+		// stem the word
+		snowballEnv.SetCurrent(word)
+		english.Stem(snowballEnv)
+		word = snowballEnv.Current()
+
+		wordToFreqency[word]++
+	}
+
 	return nil
 }
 
-func createIndex(inDB string, outDB string) error {
+func createIndex(collectionDB string, indexDB string, dictionaryDB string) error {
 	// Open db containing crawled data
-	db, err := sql.Open("sqlite", inDB)
-	defer db.Close()
-	if err != nil {
-		return err
+	cdb, cerr := sql.Open("sqlite", collectionDB)
+	defer cdb.Close()
+	if cerr != nil {
+		return cerr
 	}
 
-	// Create db for index we generate
-	os.Create(outDB)
-	db2, err2 := sql.Open("sqlite", outDB)
-	defer db2.Close()
-	if err2 != nil {
-		return err
+	// Create db for index
+	os.Create(indexDB)
+	idb, ierr := sql.Open("sqlite", indexDB)
+	defer idb.Close()
+	if ierr != nil {
+		return ierr
+	}
+
+	// Create db for dictionary
+	os.Create(dictionaryDB)
+	ddb, derr := sql.Open("sqlite", dictionaryDB)
+	defer ddb.Close()
+	if derr != nil {
+		return derr
 	}
 
 	doc := Document{0, "", "", ""}
 	for {
-		err = doc.getNextDocument(db)
+		err := doc.getNextDocument(cdb)
 		if err != nil {
 			fmt.Println(err)
 			fmt.Println("Finished indexing")
 			break
 		}
 
-		doc.index(db2)
+		doc.index(idb, ddb)
+	}
+
+	return nil
+}
+
+func loadStopWords(path string) error {
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		stopWords[scanner.Text()] = 0
+	}
+
+	err = scanner.Err()
+	if err != nil {
+		return err
 	}
 
 	return nil
 }
 
 func main() {
-	inDB := "../out/document_collection.db"
-	outDB := "../out/index.db"
+	collectionDB := "../out/document_collection.db"
+	indexDB := "../out/index.db"
+	dictionaryDB := "../out/dictionary.db"
+	stopWordsPath := "../out/stopwords.txt"
 
-	createIndex(inDB, outDB)
+	err := loadStopWords(stopWordsPath)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// checking the stop words were loaded correctly
+	// for key, val := range stopWords {
+	// 	fmt.Println(key, val)
+	// }
+
+	err = createIndex(collectionDB, indexDB, dictionaryDB)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
