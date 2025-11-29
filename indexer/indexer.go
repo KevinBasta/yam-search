ca8751981b4d35806e63a48eede858fe10b9e97a
Kevin Basta <kevinbastadeveloper@gmail.com>
2025-11-28 20:13:11 -0500

    writing out dictionary and index
    there is an err in index json data

--- indexer/indexer.go
+++ indexer/indexer.go
@@ -3,9 +3,11 @@ package main
 import (
 	"bufio"
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"os"
 	"strings"
+	"unicode"
 
 	"github.com/blevesearch/snowballstem"
 	"github.com/blevesearch/snowballstem/english"
@@ -14,6 +16,16 @@ import (
 
 var stopWords = make(map[string]int)
 var snowballEnv = snowballstem.NewEnv("")
+
+func formatWord(word *string) {
+	*word = strings.ToLower(*word)
+
+	isPunctuation := func(c rune) bool {
+		return !unicode.IsLetter(c) && !unicode.IsNumber(c)
+	}
+
+	*word = strings.TrimFunc(*word, isPunctuation)
+}
 
 type Document struct {
 	docId int
@@ -46,6 +58,12 @@ func (doc *Document) index(idb *sql.DB, ddb *sql.DB) error {
 	// loop through words in body of document
 	words := strings.Fields(doc.body)
 	for _, word := range words {
+		// trim both ends of word of non number or letter characters
+		formatWord(&word)
+		if word == "" {
+			continue
+		}
+
 		// skip this word if it's a stop word
 		_, isStopWord := stopWords[word]
 		if isStopWord {
@@ -60,36 +78,115 @@ func (doc *Document) index(idb *sql.DB, ddb *sql.DB) error {
 		wordToFreqency[word]++
 	}
 
+	// add term -> docIds and update term -> frequency in dbs
+	for word, addFrequency := range wordToFreqency {
+		// term -> frequency
+		var frequency int
+		dictionaryEntry := ddb.QueryRow("SELECT frequency FROM termToFrequency WHERE term = ?", word)
+		dictionaryErr := dictionaryEntry.Scan(&frequency)
+		if dictionaryErr != nil {
+			// handle term not being in term -> frequency
+			_, err := ddb.Exec("INSERT INTO termToFrequency(term, frequency) VALUES(?, ?)", word, addFrequency)
+			if err != nil {
+				return err
+			}
+		} else {
+			// handle term being in term -> frequency
+			frequency += addFrequency
+			_, err := ddb.Exec("INSERT INTO termToFrequency(term, frequency) VALUES(?, ?)", word, frequency)
+			if err != nil {
+				return err
+			}
+		}
+
+		// term -> docIds
+		var jsonDocIds string
+		indexEntry := idb.QueryRow("SELECT docIds FROM termToDocs WHERE term = ?", word)
+		indexErr := indexEntry.Scan(&jsonDocIds)
+		if indexErr != nil {
+			// create a json list with this docId
+			var docIds = [1]int{doc.docId}
+			updatedJsonDocIds, err := json.Marshal(docIds)
+			if err != nil {
+				return err
+			}
+
+			for _, i := range docIds {
+				fmt.Print(i)
+			}
+
+			// write out new term to docId mapping
+			_, err = idb.Exec("INSERT OR REPLACE INTO termToDocs(term, docIds) VALUES(?, ?)", word, updatedJsonDocIds)
+			if err != nil {
+				return err
+			}
+		} else {
+			// get already written docIds
+			var docIds []int
+			err := json.Unmarshal([]byte(jsonDocIds), &docIds)
+			if err != nil {
+				return err
+			}
+
+			// add this docId
+			docIds = append(docIds, doc.docId)
+			for _, i := range docIds {
+				fmt.Print(i)
+			}
+
+			updatedJsonDocIds, err := json.Marshal(docIds)
+			if err != nil {
+				return err
+			}
+
+			// write out with new docId included
+			_, err = idb.Exec("INSERT OR REPLACE INTO termToDocs(term, docIds) VALUES(?, ?)", word, updatedJsonDocIds)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
 	return nil
 }
 
 func createIndex(collectionDB string, indexDB string, dictionaryDB string) error {
 	// Open db containing crawled data
 	cdb, cerr := sql.Open("sqlite", collectionDB)
-	defer cdb.Close()
 	if cerr != nil {
 		return cerr
 	}
+	defer cdb.Close()
 
 	// Create db for index
 	os.Create(indexDB)
 	idb, ierr := sql.Open("sqlite", indexDB)
-	defer idb.Close()
 	if ierr != nil {
 		return ierr
 	}
 
+	_, err := idb.Exec("CREATE TABLE termToDocs (term TEXT PRIMARY KEY, docIds TEXT);")
+	if err != nil {
+		return err
+	}
+	defer idb.Close()
+
 	// Create db for dictionary
 	os.Create(dictionaryDB)
 	ddb, derr := sql.Open("sqlite", dictionaryDB)
-	defer ddb.Close()
 	if derr != nil {
 		return derr
 	}
 
+	_, err = ddb.Exec("CREATE TABLE termToFrequency (term TEXT PRIMARY KEY, frequency INTEGER);")
+	if err != nil {
+		return err
+	}
+	defer ddb.Close()
+
 	doc := Document{0, "", "", ""}
 	for {
-		err := doc.getNextDocument(cdb)
+		err = doc.getNextDocument(cdb)
 		if err != nil {
 			fmt.Println(err)
 			fmt.Println("Finished indexing")
