ec3434328d8cb906093af08f9c7068798c17f5b0
Kevin Basta <kevinbastadeveloper@gmail.com>
2025-12-01 12:58:56 -0500

    small file renames

--- search/search.go
+++ search/search.go
@@ -1,67 +1,253 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
+	"math"
+	"sort"
+	"strings"
 
 	"github.com/KevinBasta/yam-search/common"
+	"github.com/blevesearch/snowballstem/english"
 )
 
-var indexDB string = "../out/index.db"
-var collectionDB string = "../out/document_collection.db"
-
-type Response struct {
-	Results []searchResult `json:"results"`
+type searchResult struct {
+	DocUrl           string
+	CosineSimilarity float64
 }
 
-func searchHandler(w http.ResponseWriter, r *http.Request) {
-	query := r.URL.Query().Get("q")
+func loadTotalDocs(indexDB string) (int, error) {
+	idb, ierr := sql.Open("sqlite", indexDB)
+	if ierr != nil {
+		return 0, ierr
+	}
+	defer idb.Close()
 
-	results, err := search(indexDB, collectionDB, query)
+	var totalDocs int
+	entry := idb.QueryRow("SELECT value FROM metadata WHERE key = ?", "totalDocs")
+	err := entry.Scan(&totalDocs)
 	if err != nil {
-		fmt.Println(err)
+		return 0, err
 	}
 
-	response := Response{
-		Results: results,
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-
-	err = json.NewEncoder(w).Encode(response)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+	return totalDocs, nil
 }
 
-func main() {
-	dictionaryDB := "../out/dictionary.db"
-	stopWordsPath := "../out/stopwords.txt"
+func getPostingList(indexDB string, term string) (map[int]int, error) {
+	idb, ierr := sql.Open("sqlite", indexDB)
+	if ierr != nil {
+		return nil, ierr
+	}
+	defer idb.Close()
 
-	// Load stop words, total doc amount, and dictionary
-	err := common.LoadStopWords(stopWordsPath)
-	if err != nil {
-		fmt.Println(err)
+	var jsonPostingList string
+	indexEntry := idb.QueryRow("SELECT postingList FROM termToPostingList WHERE term = ?", term)
+	indexErr := indexEntry.Scan(&jsonPostingList)
+	if indexErr != nil {
+		return nil, indexErr
 	}
 
-	totalDocs, err := loadTotalDocs(indexDB)
+	var postingList = make(map[int]int)
+	err := json.Unmarshal([]byte(jsonPostingList), &postingList)
 	if err != nil {
-		fmt.Println(err)
+		return nil, err
 	}
-	// fmt.Println("total docs: ", totalDocs)
 
-	err = loadDictionary(dictionaryDB, totalDocs)
+	return postingList, nil
+}
+
+// map: term -> (weight = idf * tf)
+func processQuery(query string) (map[string]float64, float64, error) {
+	var wordToFreqency = make(map[string]int)
+
+	// loop through words in the query
+	words := strings.Fields(query)
+	for _, word := range words {
+		// trim both ends of word of non number or letter characters
+		common.FormatWord(&word)
+		if word == "" {
+			continue
+		}
+
+		// skip this word if it's a stop word
+		_, isStopWord := common.StopWords[word]
+		if isStopWord {
+			continue
+		}
+
+		// stem the word
+		common.SnowballEnv.SetCurrent(word)
+		english.Stem(common.SnowballEnv)
+		word = common.SnowballEnv.Current()
+
+		wordToFreqency[word]++
+	}
+
+	// calculate weight for each term in query
+	var wordToWeight = make(map[string]float64)
+	for term, freq := range wordToFreqency {
+		var tf float64 = 0
+		if freq > 0 {
+			tf = float64(1) + math.Log10(float64(freq))
+		}
+
+		wordToWeight[term] = tf * dictionary[term] // tf * idf
+	}
+
+	// calculate length of query for cosine similarity
+	var length float64
+	for _, weight := range wordToWeight {
+		length += math.Pow(weight, 2.0)
+	}
+	length = math.Sqrt(length)
+
+	return wordToWeight, length, nil
+}
+
+func search(indexDB string, collectionDB string, query string) ([]searchResult, error) {
+	// get query term weights and length
+	queryTermToWeight, queryLength, err := processQuery(query)
 	if err != nil {
-		fmt.Println(err)
+		return nil, err
 	}
-	// for key, val := range dictionary { println(key, val) }
 
-	// register search endpoint and start server on port 8080
-	http.HandleFunc("/search", searchHandler)
+	// get the posting list of each term in the query
+	var termToPostingList = make(map[string]map[int]int)
+	for term, _ := range queryTermToWeight {
+		_, inDictionary := dictionary[term]
+		if inDictionary {
+			postingList, err := getPostingList(indexDB, term)
+			if err != nil {
+				return nil, err
+			}
 
-	fmt.Println("Server starting on http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+			termToPostingList[term] = postingList
+		}
+	}
+
+	// create a slice containing the query terms
+	var sortedQueryTerms []string
+	for term, _ := range queryTermToWeight {
+		sortedQueryTerms = append(sortedQueryTerms, term)
+	}
+
+	// debug code for checking idf order before sorting
+	// for _, term := range sortedQueryTerms {
+	// 	fmt.Print(dictionary[term], " ")
+	// }
+	// fmt.Println("eof")
+
+	// sort query terms by idf
+	// switch to slices.SortFunc
+	sort.Slice(sortedQueryTerms, func(i, j int) bool {
+		iIdf, hasI := dictionary[sortedQueryTerms[i]]
+		if !hasI {
+			iIdf = 0
+		}
+
+		jIdf, hasJ := dictionary[sortedQueryTerms[j]]
+		if !hasJ {
+			jIdf = 0
+		}
+
+		return iIdf > jIdf
+	})
+
+	var docIdToCosineSimilarity = make(map[int]float64)
+	// search by highest idf term to lowest idf term
+	for _, loopTerm := range sortedQueryTerms {
+		// cut off if idf is 0
+		// idf, hasIdf := dictionary[loopTerm]
+		// if !hasIdf || (hasIdf && idf == 0) {
+		// 	continue
+		// }
+
+		// calculate the cosine similarity between a document and the query
+
+		for docId, _ := range termToPostingList[loopTerm] {
+			_, hasScore := docIdToCosineSimilarity[docId]
+			if hasScore {
+				continue
+			}
+
+			// find each term in the query that is also in this document
+			var documentWordToWeight = make(map[string]float64)
+			for _, calcTerm := range sortedQueryTerms {
+				docFrequency, hasDoc := termToPostingList[calcTerm][docId]
+
+				if hasDoc {
+					// calculate the term frequency of document
+					var tf float64 = 0
+					if docFrequency > 0 {
+						tf = float64(1) + math.Log10(float64(docFrequency))
+					}
+
+					documentWordToWeight[calcTerm] = tf * dictionary[calcTerm]
+				}
+			}
+
+			// calculate document length
+			var documentLength float64
+			for _, weight := range documentWordToWeight {
+				documentLength += math.Pow(weight, 2.0)
+			}
+			documentLength = math.Sqrt(documentLength)
+
+			// calculate cosine similarity
+			var numerator float64 = 0.0
+			for _, word := range sortedQueryTerms {
+				queryTermWeight, hasQueryTermWeight := queryTermToWeight[word]
+				documentTermWeight, hasDocumentTermWeight := documentWordToWeight[word]
+
+				// fmt.Println(queryTermWeight, documentTermWeight)
+				if hasQueryTermWeight && hasDocumentTermWeight {
+					numerator += (documentTermWeight * queryTermWeight)
+				}
+			}
+
+			var cosineSimilarity float64 = 0
+			if documentLength > 0 && queryLength > 0 {
+				cosineSimilarity = numerator / (documentLength * queryLength)
+			}
+
+			// fmt.Println(numerator, documentLength, queryLength, cosineSimilarity)
+			docIdToCosineSimilarity[docId] = cosineSimilarity
+		}
+	}
+
+	// return only top 10 results
+	cdb, cerr := sql.Open("sqlite", collectionDB)
+	if cerr != nil {
+		return nil, cerr
+	}
+	defer cdb.Close()
+
+	var pairs []searchResult
+	for docId, cosineSimilarity := range docIdToCosineSimilarity {
+		var url string
+		row := cdb.QueryRow("SELECT url FROM docIdToData WHERE docId = ?", docId)
+		err := row.Scan(&url)
+		if err != nil {
+			return nil, err
+		}
+
+		//fmt.Println(docId, cosineSimilarity)
+		pairs = append(pairs, searchResult{DocUrl: url, CosineSimilarity: cosineSimilarity})
+	}
+
+	sort.Slice(pairs, func(i, j int) bool {
+		return pairs[i].CosineSimilarity > pairs[j].CosineSimilarity
+	})
+
+	topN := 10
+	if len(pairs) < topN {
+		topN = len(pairs)
+	}
+	top10 := pairs[:topN]
+
+	// for i, pair := range pairs {
+	// 	fmt.Println(i, pair)
+	// }
+
+	return top10, nil
 }
---
+++ search/server.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/KevinBasta/yam-search/common"
+)
+
+var indexDB string = "../out/index.db"
+var collectionDB string = "../out/document_collection.db"
+
+type Response struct {
+	Results []searchResult `json:"results"`
+}
+
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query().Get("q")
+
+	results, err := search(indexDB, collectionDB, query)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	response := Response{
+		Results: results,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	err = json.NewEncoder(w).Encode(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func main() {
+	dictionaryDB := "../out/dictionary.db"
+	stopWordsPath := "../out/stopwords.txt"
+
+	// Load stop words, total doc amount, and dictionary
+	err := common.LoadStopWords(stopWordsPath)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	totalDocs, err := loadTotalDocs(indexDB)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// fmt.Println("total docs: ", totalDocs)
+
+	err = loadDictionary(dictionaryDB, totalDocs)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// for key, val := range dictionary { println(key, val) }
+
+	// register search endpoint and start server on port 8080
+	http.HandleFunc("/search", searchHandler)
+
+	fmt.Println("Server starting on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
